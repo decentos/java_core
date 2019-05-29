2290a54c60e255306905dc53742b770ac705e484
Vlad Matveev <matveev_vlad@me.com>
2019-05-29 12:54:18 +0300

    Добавил личные сообщения по логину. Проверка на дубли корректно не работает, но если второй раз авторизоваться по одному и тому же логину, то ему уже будет присвоено новое имя внутри чата, то есть в рамках чата это будут user1 и user2, если они авторизовались по login1.

--- GeekBrains_ChatServer/src/Client/Controller.java
+++ GeekBrains_ChatServer/src/Client/Controller.java
@@ -3,8 +3,10 @@ package Client;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -14,7 +16,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class Controller implements Initializable {
+public class Controller{
     @FXML
     TextArea textArea;
 
@@ -28,11 +30,41 @@ public class Controller implements Initializable {
     DataInputStream in;
     DataOutputStream out;
 
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    TextField loginFiled;
+
+    @FXML
+    PasswordField passwordField;
+
+    private boolean isAuthorized;
+
     final String IP_ADRESS = "localhost";
     final int PORT = 8189;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    // метод для показа нижней панели или верхней
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+
+        if(!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+        }
+    }
+
+    public void connect() {
         try {
             socket = new Socket(IP_ADRESS, PORT);
             in = new DataInputStream(socket.getInputStream());
@@ -42,8 +74,23 @@ public class Controller implements Initializable {
                 @Override
                 public void run() {
                     try {
+                        // блок для авторизации
                         while (true) {
                             String str = in.readUTF();
+                            if(str.startsWith("/authok")) {
+                                setAuthorized(true);
+                                break;
+                            } else {
+                                textArea.appendText(str + "\n");
+                            }
+                        }
+
+                        // блок для разбора сообщений
+                        while (true) {
+                            String str = in.readUTF();
+                            if(str.equals("/serverClosed")) {
+                                break;
+                            }
                             textArea.appendText(str + "\n");
                         }
                     } catch (IOException e) {
@@ -54,6 +101,7 @@ public class Controller implements Initializable {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+                        setAuthorized(false);
                     }
                 }
             }).start();
@@ -63,6 +111,23 @@ public class Controller implements Initializable {
         }
     }
 
+    // метод для авторизации
+    public void tryToAuth() {
+        if(socket == null || socket.isClosed()) {
+            // сначала подключаемся к серверу
+            connect();
+        }
+        try {
+            // отправка сообщений на сервер для авторизации
+            out.writeUTF("/auth " + loginFiled.getText() + " " + passwordField.getText());
+            loginFiled.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // метод для отправки сообщений
     public void sendMsg() {
         try {
             out.writeUTF(textField.getText());
--- GeekBrains_ChatServer/src/Server/ClientHandler.java
+++ GeekBrains_ChatServer/src/Server/ClientHandler.java
@@ -4,31 +4,77 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Vector;
 
 public class ClientHandler {
     private DataInputStream in;
     private DataOutputStream out;
     private Socket socket;
     private MainServer server;
+    private String nick;
+    private String clientName;
+
+    private static int counter = 0;
 
     public ClientHandler(Socket socket, MainServer server) {
         try {
+            counter++;
+            this.clientName = "user" + counter;
             this.socket = socket;
             this.server = server;
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
+            // IDEA подсказала, что можно записать все через лямбду
+            new Thread(() -> {
                     try {
+                        // цикл для авторизации
+                        while (true) {
+                            String str = in.readUTF();
+                            // если сообщение начинается с /auth
+                            if(str.startsWith("/auth")) {
+                                String[] tokens = str.split(" ");
+                                // Вытаскиваем данные из БД
+                                String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+
+                                // Проверка дублей // НЕ РАБОТАЕТ
+                                if (server.repeatLogin(tokens[1])){
+                                    System.out.println("Пользователь уже авторизован!");
+                                }
+
+                                if (newNick != null) {
+                                    // отправляем сообщение об успешной авторизации
+                                    sendMsg("/authok");
+                                    nick = newNick;
+                                    server.subscribe(ClientHandler.this);
+                                    break;
+                                } else {
+                                    sendMsg("Неверный логин/пароль!");
+                                }
+                            }
+                        }
+
+                        // блок для отправки сообщений
                         while (true) {
                             String str = in.readUTF();
                             if(str.equals("/end")) {
+                                out.writeUTF("/serverClosed");
                                 break;
                             }
-                            //out.writeUTF("Client: " + str + " echo!");
-                            server.broadcastMsg("Client: " + str);
+
+                            // Личные сообщения:
+                            if(str.startsWith("/w")) {
+                                String to = str.split(" ")[1];
+                                //String msg = str.split(" ")[2]; // так отправляется только первое слово
+                                server.wisperMsg(this, to, str); // сообщение выбранным
+
+                                // если использовать str вместо msg в третьем аргументе, то отправляется все сообщение
+                                // но при этом служебная команда "/w user2" тоже является частью сообщения
+
+                            } else {
+                                server.broadcastMsg("[" + this.clientName + "] " + str); //сообщение для всех
+                            }
+                            out.flush();
                         }
                     }  catch (IOException e) {
                         e.printStackTrace();
@@ -48,8 +94,8 @@ public class ClientHandler {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+                        server.unsubscribe(ClientHandler.this);
                     }
-                }
             }).start();
 
         } catch (IOException e) {
@@ -63,5 +109,9 @@ public class ClientHandler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getClientName() {
+        return this.clientName;
     }
 }
